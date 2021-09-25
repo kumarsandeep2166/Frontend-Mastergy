ca681ed77f0a48533edc48262e7287ca4246ca68
Pratik Agarwal <pratik1234agarwal@gmail.com>
2021-09-25 19:21:40 +0530

    Addded changes

--- src/State/Actions/user.js
+++ src/State/Actions/user.js
@@ -1,31 +1,33 @@
-import axios from "axios";
-import { stopLoading, setLoading } from "./loading";
+import axios from 'axios';
+import { stopLoading, setLoading } from './loading';
 
 export const userSignUp = (user, history) => {
   return async (dispatch) => {
-    console.log("action", user);
+    console.log('action', user);
     try {
+      console.log('user', user);
       setLoading(dispatch);
-      console.log("Set Loading to true");
-      const res = await axios.post("/pilot/accounts/signup", {
+      console.log('Set Loading to true');
+      const res = await axios.post('/pilot/accounts/signup', {
         name: user.username,
-        email: user.useremail,
+        email: user.email,
         password: user.password,
         phone_number: user.userphone,
         user_role: user.user_role,
       });
-      console.log("user_role", user.user_role);
+      console.log(res.data);
+      console.log('user_role', user.user_role);
 
-      console.log("Data fetching done");
+      console.log('Data fetching done');
       dispatch({
-        type: "USER_LOGIN",
+        type: 'USER_LOGIN',
         payload: { ...res.data.data },
       });
       dispatch({
-        type: "REMEMBER_ME",
+        type: 'REMEMBER_ME',
       });
       if (parseInt(user.user_role) === 1) {
-        const temp = await axios.post("/pilot/accounts/addorganisation", {
+        const temp = await axios.post('/pilot/accounts/addorganisation', {
           name: user.username,
           user: res.data.data.id,
           lat: 50,
@@ -35,9 +37,9 @@ export const userSignUp = (user, history) => {
       }
       stopLoading(dispatch);
       if (parseInt(user.user_role) === 1) {
-        history.push("/organizationprofile");
+        history.push('/organizationprofile');
       }
-      alert("Account Successfully created!! Redirecting to your profile");
+      alert('Account Successfully created!! Redirecting to your profile');
     } catch (err) {
       stopLoading(dispatch);
       alert(err.response.data.error.message);
@@ -45,43 +47,43 @@ export const userSignUp = (user, history) => {
   };
 };
 
-export const userLoginIn = ({ email, password, rememberUser }) => async (
-  dispatch
-) => {
-  setLoading(dispatch);
-  try {
-    const res = await axios.post("/pilot/accounts/login", {
-      email,
-      password,
-    });
-    if (rememberUser) {
-      dispatch({
-        type: "REMEMBER_ME",
+export const userLoginIn =
+  ({ email, password, rememberUser }) =>
+  async (dispatch) => {
+    setLoading(dispatch);
+    try {
+      const res = await axios.post('/pilot/accounts/login', {
+        email,
+        password,
       });
-    } else {
+      if (rememberUser) {
+        dispatch({
+          type: 'REMEMBER_ME',
+        });
+      } else {
+        dispatch({
+          type: 'ONE_TIME',
+        });
+      }
+      console.log(res.data);
       dispatch({
-        type: "ONE_TIME",
+        type: 'USER_LOGIN',
+        payload: { ...res.data.data },
       });
+      stopLoading(dispatch);
+    } catch (err) {
+      console.log(err);
+      stopLoading(dispatch);
+      alert(err.response.data.error.message);
     }
-    console.log(res.data);
-    dispatch({
-      type: "USER_LOGIN",
-      payload: { ...res.data.data },
-    });
-    stopLoading(dispatch);
-  } catch (err) {
-    console.log(err);
-    stopLoading(dispatch);
-    alert(err.response.data.error.message);
-  }
-};
+  };
 
 export const refreshUserState = () => async (dispatch) => {
   try {
     setLoading(dispatch);
-    const res = await axios.get("/pilot/accounts/myprofile");
+    const res = await axios.get('/pilot/accounts/myprofile');
     dispatch({
-      type: "USER_LOGIN",
+      type: 'USER_LOGIN',
       payload: { ...res.data },
     });
     stopLoading(dispatch);
@@ -97,7 +99,7 @@ export const verifyOtp = (otp) => async (dispatch) => {
     await axios.put(`/pilot/accounts/verifyotp?url_otp=${otp}`);
     refreshUserState();
     dispatch({
-      type: "OTP_VERIFIED",
+      type: 'OTP_VERIFIED',
     });
   } catch (err) {
     console.log(err);
